e8b3d39a6bee2d58410e242de0f8f1992064c628
Luke Zeches <zechesl@gmail.com>
2023-10-17 15:19:55 -0400

    Created method for getting vertex coords from file

--- GraphUtility.java
+++ GraphUtility.java
@@ -4,10 +4,17 @@ import java.util.Scanner;
 
 public class GraphUtility {
 
+    public static void main(String[] args) {
+        try {
+            int[][] coords = VertexCoordsFromfile(new File("./graphs/simpleCoords.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+
     // Get adjacency matrix from file
     public static int[][] AdjMatrixFromFile(File f) throws FileNotFoundException {
-        Scanner rowReader;
-        rowReader = new Scanner(f);
+        Scanner rowReader = new Scanner(f);
         int rowIndex = 0;
         int[][] adjacencyMatrix = null;
         while (rowReader.hasNextLine()) {
@@ -20,7 +27,7 @@ public class GraphUtility {
             if (adjacencyMatrix == null) {
                 adjacencyMatrix = new int[vals.length][vals.length];
             }
-            // Put values from
+            // Put values from file into matrix
             for (int i = 0; i < vals.length; i++) {
                 adjacencyMatrix[rowIndex][i] = Integer.parseInt(vals[i]);
                 System.out.print(vals[i] + " ");
@@ -32,6 +39,28 @@ public class GraphUtility {
         }
         rowReader.close();
         return adjacencyMatrix;
+    }
 
+    public static int[][] VertexCoordsFromfile(File f) throws FileNotFoundException {
+        // Coordinate files will be in the following format:
+        /*
+         * N (where N = number of vertices)
+         * X0 Y0
+         * X1 Y1
+         * ...
+         * XN YN
+         */
+        Scanner rowReader = new Scanner(f);
+        int numVertices = rowReader.nextInt();
+        rowReader.nextLine();
+        int[][] vertexCoords = new int[numVertices][2];
+        // Read each row and pass the numbers into the coordinates array
+        for (int i = 0; i < numVertices; i++) {
+            String[] row = rowReader.nextLine().split(" ");
+            vertexCoords[i][0] = Integer.parseInt(row[0]);
+            vertexCoords[i][1] = Integer.parseInt(row[1]);
+        }
+        rowReader.close();
+        return vertexCoords;
     }
 }

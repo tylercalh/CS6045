51ff06f70a3c7baf96f20ba2da19659bb1ce38db
Tyler Calhoun <tylercalhoun@Tylers-MacBook-Pro.local>
2023-11-22 16:12:51 -0500

    Remove Graph.node, Add Graph.buildNodes()

--- Graph.java
+++ Graph.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 
 public class Graph {
     double[][] adjacency_matrix;
-    HashMap<Integer, PathNode> nodes;
 
     // If only one file path is supplied then the resulting Graph will be unweighted.
     // adj_filepath -- a String representing the file path of an adj.txt. For example "./graphs/medium/adj.txt"
@@ -14,7 +13,7 @@ public class Graph {
         try {
             double[][] adjacency_matrix = GraphUtility.AdjMatrixFromFile(adj_file);
             this.adjacency_matrix = adjacency_matrix;
-            this.nodes = buildNodes(adjacency_matrix);
+            //this.nodes = buildNodes(adjacency_matrix);
         } catch (FileNotFoundException fnfe) {
             System.err.println(fnfe);
         }
@@ -32,15 +31,15 @@ public class Graph {
             double[][] adjacency_matrix = GraphUtility.AdjMatrixFromFile(adj_file);
 
             this.adjacency_matrix = GraphUtility.WeightedAdjMatrixFromCoords(adjacency_matrix, vertexCoords);
-            this.nodes = buildNodes(this.adjacency_matrix);
+            //this.nodes = buildNodes(this.adjacency_matrix);
         } catch (FileNotFoundException fnfe) {
             System.err.println(fnfe);
         }
     }
 
-    private HashMap<Integer, PathNode> buildNodes(double[][] adjacency_matrix) {
-        nodes = new HashMap<>();
-        for (int i = 0; i < adjacency_matrix[0].length; i++) {
+    public HashMap<Integer, PathNode> buildNodes() {
+        HashMap<Integer, PathNode> nodes = new HashMap<>();
+        for (int i = 0; i < this.adjacency_matrix[0].length; i++) {
             nodes.put(i, new PathNode(i));
         }
         return nodes;

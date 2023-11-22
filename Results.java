69a951627e9257ff16f18dae2781d48e119df437
Luke Zeches <zechesl@gmail.com>
2023-11-22 13:02:33 -0500

    Created Results class

---
+++ Results.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+// Class for storing the results of a pathfinding method
+public class Results {
+    private ArrayList<Integer> path; // An ArrayList of integers holding the 
+    private int numNodesExplored;
+    private double pathLength;
+
+    public Results() {
+        this.path = null;
+        this.numNodesExplored = 0;
+        this.pathLength = 0;
+    }
+    public Results(ArrayList<Integer> path, int numNodesExplored, double pathLength){
+        this.path = path;
+        this.numNodesExplored = numNodesExplored;
+        this.pathLength = pathLength;
+    }
+
+    // Setters
+    public void setPath(ArrayList<Integer> path) {
+        this.path = path;
+    }
+    public void setNumNodesExplored(int numNodesExplored) {
+        this.numNodesExplored = numNodesExplored;
+    }
+
+    public void setPathLength(double pathLength) {
+        this.pathLength = pathLength;
+    }
+
+    // Getters
+
+
+    public ArrayList<Integer> getPath() {
+        return path;
+    }
+
+    public int getNumNodesExplored() {
+        return numNodesExplored;
+    }
+
+    public double getPathLength() {
+        return pathLength;
+    }
+}

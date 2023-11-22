dbb6bf9d3cafc64c9e5a3040af1a9cb560e0527b
Luke Zeches <zechesl@gmail.com>
2023-11-22 16:25:11 -0500

    Add start and end indices to findPath()

--- IPathFinder.java
+++ IPathFinder.java
@@ -1,5 +1,6 @@
 // All pathfinder algorithm classes must implement this
 public interface IPathFinder {
     // The only method from the algorithm's class that needs to be called from an external class
-    public Results findPath(Graph g); // Will throw an error until the Graph class is created by Tyler
+    // start and goal are the indices of the start node of the path and the end node of the path
+    public Results findPath(Graph g, int start, int goal);
 }

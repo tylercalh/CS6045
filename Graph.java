7201178eef6bc3b4be802645ec2f3bb501e705c5
Tyler Calhoun <tylercalhoun@Tylers-MacBook-Pro.local>
2023-11-23 17:15:52 -0500

    Add w_adj_matrix and coordinates to Graph.

--- Graph.java
+++ Graph.java
@@ -5,15 +5,29 @@ import java.util.HashMap;
 public class Graph {
     // Adjacency matrix representing vertices and edges.
     double[][] adjacency_matrix;
+    // Weighted adjacency matrix representing weighted vertices and edges.
+    double[][] w_adjacency_matrix;
+    // coordinates are stored in this form:
+    // [[x1, y1],[x2,y2],[x3,y3],...[xn, yn]]
+    int[][] coordinates;
 
-    // If only one file path is supplied then the resulting Graph will be unweighted.
-    // adj_filepath -- a String representing the file path of an adj.txt. For example "./graphs/medium/adj.txt"
-    public Graph(String adj_filepath) {
+    // adj_filepath -- a String representing the file path of an adj.txt. For example "./more_graphs/realistic_medium/adj.txt"
+    // wadj_filepath -- a String representing the file path of a wadj.txt. For example "./more_graphs/realistic_medium/wadj.txt"
+    // coord_filepath -- a String representing the file path of a coords.txt file. For example "./more_graphs/realistic_medium/coords.txt"
+    public Graph(String adj_filepath, String wadj_filepath, String coord_filepath) {
         File adj_file = new File(adj_filepath);
+        File wadj_file = new File(wadj_filepath);
+        File coord_file = new File(coord_filepath);
 
         try {
             double[][] adjacency_matrix = GraphUtility.AdjMatrixFromFile(adj_file);
             this.adjacency_matrix = adjacency_matrix;
+
+            double[][] w_adjacency_matrix = GraphUtility.AdjMatrixFromFile(wadj_file);
+            this.w_adjacency_matrix = w_adjacency_matrix;
+
+            int[][] coordinates = GraphUtility.VertexCoordsFromfile(coord_file);
+            this.coordinates = coordinates;
         } catch (FileNotFoundException fnfe) {
             System.err.println(fnfe);
         }
@@ -22,7 +36,7 @@ public class Graph {
     // If two file paths are supplied the resulting Graph will be weighted.
     // adj_filepath -- a String representing the file path of an adj.txt. For example "./graphs/medium/adj.txt"
     // coord_filepath -- a String representing the file path of a coord.txt. For example "./graphs/medium/coord.txt"
-    public Graph(String adj_filepath, String coord_filepath) {
+    /* public Graph(String adj_filepath, String coord_filepath) {
         File adj_file = new File(adj_filepath);
         File coord_file = new File(coord_filepath);
 
@@ -34,7 +48,7 @@ public class Graph {
         } catch (FileNotFoundException fnfe) {
             System.err.println(fnfe);
         }
-    }
+    } */
 
     // Build a map of pathnodes to maintain state values necessary throughout the path finding computation.
     // find_path() consumes nodes, it is no longer useful for subsequent runs.
